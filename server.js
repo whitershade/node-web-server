38a01c56a0e64dc6c623f2229a2a23097fc964fa
whitershade <whitershade34@gmail.com>
2018-01-11 01:45:11 +0200

    added static html page

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.send('Hello Express!');
 });
 
-app.listen(3000);
-
+app.listen(3000, () => {
+  console.log('server is up on localhost:3000');
+});
