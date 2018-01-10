ca4893dbb29f863bd988e02ab926f733906e61f6
whitershade <whitershade34@gmail.com>
2018-01-11 01:59:13 +0200

    added handlebars as view engine

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello Express!');
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome here, dude!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.listen(3000, () => {
