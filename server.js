c945c11de919df211857dee636d3fc94004686c4
whitershade <whitershade34@gmail.com>
2018-01-11 02:15:58 +0200

    added hbs helpers

--- server.js
+++ server.js
@@ -4,21 +4,22 @@ const hbs = require('hbs');
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', text => text.toUpperCase());
+
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Welcome here, dude!'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
